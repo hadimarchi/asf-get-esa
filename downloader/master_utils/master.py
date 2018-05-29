fe633a864ffd6868da0b6818ac01097918a8a80f
Hal D <hal.dimarchi55@gmail.com>
2018-05-29 11:12:12 -0800

    rearranged a class

--- downloader/master_utils/master.py
+++ downloader/master_utils/master.py
@@ -29,10 +29,20 @@ class Master:
         log.critical("Received a SIGTERM, cleaning up and shutting down")
         sys.exit(0)
 
-    def get_product_ids_from_url(self):
-        for product in range(len(self.products)):
-            self.products[product] = get_product_from_url(
-                                        self.products[product])
+    def idle(self):
+        try:
+            while self.options.run:
+                check_and_clean_log_file()
+                log.info("Spinning up download cycle")
+                self.get_products_from_db()
+                if self.products:
+                    self.download_products()
+                else:
+                    wait(self.options.wait_period)
+                self.options.update_max_processes_and_run()
+            log.info("run option is not 1, exiting")
+        finally:
+            self.options.set_running('0')
 
     def get_products_from_db(self):
         self.products = self.sql.get_granules()
@@ -40,32 +50,10 @@ class Master:
 
         log.info(f'Found {len(self.products)} products for this run.')
 
-    def reset_products_not_downloaded(self):
-        try:
-            log.info("Reseting failed products downloaded status to false.")
-
-            self.sql.cleanup(self.failed_products)
-            log.info("Reset failed_products.")
-        except Exception:
-            log.error("An error occurred with the database. Retrying reset")
-            try:
-                self.sql.cleanup(self.failed_products)
-
-            except Exception as e:
-                log.error("Granules could not be reset, download status in database may be innaccurate for some granules")
-                log.error(f"Error was: {str(e)}")
-            else:
-                log.error("Reset retry successful")
-
-        except KeyboardInterrupt:
-            log.info("Keyboard interrupt ignored. Need to reset failed products in db")
-            self.reset_products_not_downloaded()
-
-    def get_failed_products(self):
-        log.info("Number of successfully downloaded products: {}".format(len(self.children.successful_granules_list)))
-        self.failed_products = [product for product in self.failed_products if
-                                product not in self.children.successful_granules_list]
-        log.info(f"Number of failed products: {len(self.failed_products)}")
+    def get_product_ids_from_url(self):
+        for product in range(len(self.products)):
+            self.products[product] = get_product_from_url(
+                                        self.products[product])
 
     def download_products(self):
         try:
@@ -96,17 +84,29 @@ class Master:
                 self.reset_products_not_downloaded()
                 self.failed_products = []
 
-    def idle(self):
+    def get_failed_products(self):
+        log.info("Number of successfully downloaded products: {}".format(len(self.children.successful_granules_list)))
+        self.failed_products = [product for product in self.failed_products if
+                                product not in self.children.successful_granules_list]
+        log.info(f"Number of failed products: {len(self.failed_products)}")
+
+    def reset_products_not_downloaded(self):
         try:
-            while self.options.run:
-                check_and_clean_log_file()
-                log.info("Spinning up download cycle")
-                self.get_products_from_db()
-                if self.products:
-                    self.download_products()
-                else:
-                    wait(self.options.wait_period)
-                self.options.update_max_processes_and_run()
-            log.info("run option is not 1, exiting")
-        finally:
-            self.options.set_running('0')
+            log.info("Reseting failed products downloaded status to false.")
+
+            self.sql.cleanup(self.failed_products)
+            log.info("Reset failed_products.")
+        except Exception:
+            log.error("An error occurred with the database. Retrying reset")
+            try:
+                self.sql.cleanup(self.failed_products)
+
+            except Exception as e:
+                log.error("Granules could not be reset, download status in database may be innaccurate for some granules")
+                log.error(f"Error was: {str(e)}")
+            else:
+                log.error("Reset retry successful")
+
+        except KeyboardInterrupt:
+            log.info("Keyboard interrupt ignored. Need to reset failed products in db")
+            self.reset_products_not_downloaded()
